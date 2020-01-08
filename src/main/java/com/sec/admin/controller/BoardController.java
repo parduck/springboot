0c24b4e5f857aa1359ea34ed909d2c0f58efbc6b
chankim <videoal@gmail.com>
2020-01-08 09:32:03 +0900

    work well..

--- src/main/java/com/sec/admin/controller/BoardController.java
+++ src/main/java/com/sec/admin/controller/BoardController.java
@@ -2,7 +2,7 @@ package com.sec.admin.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//is this work well??
 @RestController
 public class BoardController {
 
