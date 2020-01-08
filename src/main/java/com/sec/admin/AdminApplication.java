d426fc61aa5e16c54ceabcdeba3181d03e289441
chankim <videoal@gmail.com>
2020-01-08 14:37:30 +0900

    lombok library added

--- src/main/java/com/sec/admin/AdminApplication.java
+++ src/main/java/com/sec/admin/AdminApplication.java
@@ -1,9 +1,12 @@
 package com.sec.admin;
 
+import org.springframework.context.annotation.ComponentScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.sec.admin", "com.sec"} )
 public class AdminApplication {
  
 	public static void main(String[] args) { 
--- src/main/java/com/sec/admin/controller/BoardController.java
+++ src/main/java/com/sec/admin/controller/BoardController.java
@@ -1,8 +1,16 @@
 package com.sec.admin.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sec.admin.domain.BoardVO;
 //is this work well??
+import com.sec.admin.domain.BoardVO;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 @RestController
 public class BoardController {
 
@@ -13,6 +21,35 @@ public class BoardController {
 	@GetMapping("/hello")
 	public String hello(String name) {
 		return "Hello:" + name; //test
+	}
+	
+	@GetMapping("/getBoard")
+	public BoardVO getBoard() {
+		BoardVO board = new BoardVO();
+		board.setSeq(1);
+		board.setTitle("test");
+		board.setWriter("kim");
+		board.setContent("test contents...");
+		board.setCreateDate(new Date());
+		board.setCnt(0);
+		return board;
+	}
+	
+	@GetMapping("/getBoardList")
+	public List<BoardVO> getBoardList() {
+		List<BoardVO> boardList = new ArrayList<BoardVO>();
+		for (int i=1;i<=10;i++) {
+			BoardVO board = new BoardVO();
+		
+			board.setSeq(i);
+			board.setTitle("test"+i);
+			board.setWriter("kim");
+			board.setContent(i+ "번 test contents...");
+			board.setCreateDate(new Date());
+			board.setCnt(0);
+			boardList.add(board);
+		}
+		return boardList;
 	}
 }
  
